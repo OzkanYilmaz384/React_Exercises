be7ede1edf175fe9828b671eef96fb13d08b37fe
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-03-23 14:53:59 +0300

    Components_02

---
+++ React_Exercises/my-app/src/components_02.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Component } from "react";
+
+export default class Hello extends Component {
+    render() {
+        return (
+            <h1>Hello, World!</h1>
+        )
+    }
+}
