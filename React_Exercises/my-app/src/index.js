e536af38f80723acdf1e2523a1174d3d62ece323
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-03-24 15:59:45 +0300

    components_04

---
+++ React_Exercises/my-app/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import App from "./components_03";
+import { ReactDOM } from "react-dom";
+
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App />);
+
