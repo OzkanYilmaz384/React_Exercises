fdad289d5808a8aad6b1d15b2874508e3458d24c
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-03-24 14:56:22 +0300

    components_03

---
+++ React_Exercises/my-app/src/components_03.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Hello from "./components_02";
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <Hello />
+            </div>
+        )
+    }
+}
+
+
