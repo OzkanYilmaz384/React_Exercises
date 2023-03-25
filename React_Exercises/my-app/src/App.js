89a67d094facfd0d5926d9fdc4f8ba2acb303981
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-03-25 17:15:13 +0300

    Props_04

--- React_Exercises/my-app/src/App.js
+++ React_Exercises/my-app/src/App.js
@@ -10,10 +10,13 @@ export default class App extends Component {
                 <Hello />
                 <Message />
 
-                <Welcome name="Ozkan" age={35} />
+                <Welcome name={<strong>Ozkan</strong>} age={35} />
             </div>
         )
     }
 }
+
+/* If want to pass a prop that contains a JSX expression or anything instead of a string,
+    we have to use {}(curly brackets) in place of "".  */
 
 
