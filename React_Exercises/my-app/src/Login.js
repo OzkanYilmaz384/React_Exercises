49ebaa0a2ca6b367bfdf9460267950ad0ccc2c5b
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-03-30 14:26:34 +0300

    Forms - 03

--- React_Exercises/my-app/src/Login.js
+++ React_Exercises/my-app/src/Login.js
@@ -1,5 +1,9 @@
 import { Component } from "react";
 
+const onLogin = () => {
+        
+}
+
 class Login extends Component {
     // constructor(props) {
     //     super(props);
@@ -8,20 +12,29 @@ class Login extends Component {
         username: "",
         password: "",
         remember: false,
+        login: true
+           
      }
+
 
     handleChanged = (event) => {
         const target = event.target;
         const value = target.type === "checkbox" ? target.checked : target.value;
         const name = target.name;
-
+        
         this.setState({
-            [name]: value,
-        })
+            [name]: value,   
+            login: (this.state.username === "" || this.state.password === "") ? true : false,
+        }    
+        );
+        
+        
     }
+    
 
     componentDidUpdate() {
         console.log(this.state);
+
     }
 
     render() { 
@@ -30,6 +43,8 @@ class Login extends Component {
                 <input type="text" name="username" value={this.state.username} onChange={this.handleChanged}/>
                 <input type="password" name="password" value={this.state.password} onChange={this.handleChanged}/>
                 <input type="checkbox" name="remember" checked={this.state.remember} onChange={this.handleChanged}/>
+                
+                <button type="button" name="login" disabled={this.state.login} onClick={onLogin}>Login</button>
             </form>
          );
     }
