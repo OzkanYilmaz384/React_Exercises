8caaf8219dacf8b589f2947f335c79f1ada3fdb7
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-04-01 02:28:49 +0300

    Styling Components - 03

--- React_Exercises/my-app/src/Login.js
+++ React_Exercises/my-app/src/Login.js
@@ -16,6 +16,8 @@ class Login extends Component {
            
      }
 
+    
+
 
     handleChanged = (event) => {
         const target = event.target;
@@ -23,12 +25,10 @@ class Login extends Component {
         const name = target.name;
         
         this.setState({
-            [name]: value,   
+            [name]: value, 
             login: (this.state.username === "" || this.state.password === "") ? true : false,
         }    
-        );
-        
-        
+        );   
     }
     
 
@@ -38,13 +38,18 @@ class Login extends Component {
     }
 
     render() { 
+
+        const InputStyle = {
+            backgroundColor: this.state.password.length < 8 ? "red" : "green",
+        }
+
         return ( 
             <form>
                 <input type="text" name="username" value={this.state.username} onChange={this.handleChanged}/>
                 <input type="password" name="password" value={this.state.password} onChange={this.handleChanged}/>
                 <input type="checkbox" name="remember" checked={this.state.remember} onChange={this.handleChanged}/>
                 
-                <button type="button" name="login" disabled={this.state.login} onClick={onLogin}>Login</button>
+                <button type="button" name="login" disabled={this.state.login} onClick={onLogin} style={InputStyle}>Login</button>
             </form>
          );
     }
