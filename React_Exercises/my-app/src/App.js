cf5b65a932f96af2c261d5d5cef37f9228f954f2
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-03-31 16:12:09 +0300

    Lists - 03

--- React_Exercises/my-app/src/App.js
+++ React_Exercises/my-app/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 
-import { UnControlledLogin } from "./UnControlledLogin";
-import { Colors } from "./Colors";
+import TodoList from "./TodoList";
 
 
 
@@ -9,8 +8,7 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                <UnControlledLogin />
-                <Colors items={[{id: 1, name: "blue"},{id: 2, name: "red"},{id: 3, name: "green"}]}/>
+                <TodoList />
             </div>
         )
     }
---
+++ React_Exercises/my-app/src/TodoList.js
@@ -0,0 +1,34 @@
+import { Component, createRef } from "react"
+
+
+class TodoList extends Component {
+   
+    state = { 
+      items: ["blue", "red", "green"],
+     }
+
+    render() { 
+        const inputRef = createRef();
+
+       const addToList = () => {
+        const item = inputRef.current.value;
+            this.setState({
+                items: [...this.state.items, item],
+            });
+         }
+
+        return ( 
+            <div>
+                <ul>
+                    {this.state.items.map((item) => <li>{item}</li> )}
+                </ul>
+                
+                <input name="input" type="text" ref={inputRef}></input>
+                <button onClick={addToList}>Click to Add!</button>
+                
+            </div>
+         );
+    }
+}
+ 
+export default TodoList;
