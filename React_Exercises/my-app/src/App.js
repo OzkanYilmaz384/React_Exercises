150d674a75adc4e9476903bd203f840f0f2a06d3
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-04-01 03:36:38 +0300

    Component_Composition_01

--- React_Exercises/my-app/src/App.js
+++ React_Exercises/my-app/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 
-import Login from "./Login";
+import { Container } from "./Container";
 
 
 export default class App extends Component {
     render() {
         return (
             <div>
-                <Login />
+                <Container>
+                    <h2>Hello World</h2>
+                </Container>
             </div>
         )
     }
