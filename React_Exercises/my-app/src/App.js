269cd718c4b8b37e1bff0602deae5d5c63429ac5
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-04-04 12:03:01 +0300

    useEffect - 02

--- React_Exercises/my-app/src/App.js
+++ React_Exercises/my-app/src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from "react";
-import Hello from "./Hello";
-import Message from "./Message";
-import { Welcome } from "./Welcome";
+
 import { Counter } from "./Counter";
-// import Age from "./Age";
 
 export default class App extends Component {
+
     render() {
+
         return (
-            <div>
-                <Hello />
-                <Message />
-                <Counter />
-                <Welcome name="John" age={15} />
-            </div>
+        <div>
+            
+            <Counter />
+
+        </div>
+            
         )
     }
 }
--- React_Exercises/my-app/src/Counter.js
+++ React_Exercises/my-app/src/Counter.js
@@ -1,20 +1,58 @@
-import { Component } from "react";
+import React, { useEffect, useState } from "react"
 
-export class Counter extends Component {
-    state = {
-        count: 0
+export function Counter () {
+    const [count, setCount] = useState(0);
+
+    function onCounterChange(count)  {
+        console.log(`Current count value is : ${count}`);
     }
 
-    constructor(props) {
-        super(props)
 
-        setInterval(() => {
-            this.setState((state) => {
-                return {count: this.state.count + 1}
-            })
-        }, 1000)
-    }
-    render() {
-        return <h1>Count : {this.state.count}</h1>
-    }
-}
+    useEffect(() => {
+        onCounterChange(count);
+      }, [count]);
+    
+    useEffect(() => {
+        const i = setInterval(() => {
+          console.log("Interval");
+        }, 1000);
+    
+        return () => {
+          clearInterval(i);
+        };
+      }, []);
+    
+      return (
+        <div>
+          <button
+            onClick={() => {
+              setCount(c => c + 1);
+            }}
+          >
+            Click me
+          </button>
+          <p>You have clicked {count} times</p>
+        </div>
+      );
+    };
+
+// import { Component } from "react";
+
+// export class Counter extends Component {
+//     state = {
+//         count: 0
+//     }
+
+//     constructor(props) {
+//         super(props)
+
+//         setInterval(() => {
+//             this.setState((state) => {
+//                 return {count: this.state.count + 1}
+//             })
+//         }, 1000)
+//     }
+//     render() {
+//         return <h1>Count : {this.state.count}</h1>
+//     }
+// }
