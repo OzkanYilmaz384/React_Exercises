b6e96fb641f09f105343f86bd54bc628fec47940
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-04-16 00:49:47 +0300

    SWR_02

--- React_Exercises/my-app/src/useGithubUser.js
+++ React_Exercises/my-app/src/useGithubUser.js
@@ -3,9 +3,11 @@ import useSWR from "swr";
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export const useGithubUser = ({username}) => {
-    
-    const {data: user, error, loading} = useSWR(`https://api.github.com/users/${username}`, fetcher)
+    const shouldRequest = username !== null;
+
+    const {data: user, error, loading} = useSWR(shouldRequest 
+        ? `https://api.github.com/users/${username}` 
+        : null, fetcher);
 
     return [user, loading, error];
-
 }
