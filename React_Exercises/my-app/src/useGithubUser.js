bade15778f98bac2f2062de02864fc1e25d1d0bf
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-04-16 01:33:58 +0300

    SWR_03

--- React_Exercises/my-app/src/useGithubUser.js
+++ React_Exercises/my-app/src/useGithubUser.js
@@ -5,9 +5,15 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 export const useGithubUser = ({username}) => {
     const shouldRequest = username !== null;
 
-    const {data: user, error, loading} = useSWR(shouldRequest 
+    const {data: user, error, isValidating, mutate} = useSWR(shouldRequest 
         ? `https://api.github.com/users/${username}` 
         : null, fetcher);
 
-    return [user, loading, error];
+    const loading = !user && !error && isValidating;
+
+    const refetch = () => {
+        mutate();
+    }
+
+    return [user, loading, error, refetch];
 }
