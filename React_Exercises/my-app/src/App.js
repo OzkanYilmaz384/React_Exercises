d53c6e800eb8aadd6d1dd573170457dedec41891
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-04-15 14:43:44 +0300

    React Router - 04

--- React_Exercises/my-app/src/App.js
+++ React_Exercises/my-app/src/App.js
@@ -2,19 +2,25 @@
 import { ClickCounter } from "./ClickCounter";
 import ShowGithubUser from "./ShowGithubUser";
 import { Welcome } from "./Welcome";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 
 export function App () {
 
 
     return (
         <div>
+            <ul>
+                    <li><Link to="/">Home</Link> </li>
+                    <li><Link to="/counter">Counter</Link></li>
+                    <li><Link to="/users/:username">User Name</Link></li>
+            </ul>
             <Routes>
                 <Route path="/" element={<Welcome name="John" />} />
 
                 <Route path="/counter" element={<ClickCounter />} />
 
                 <Route path="/users/:username" element={<ShowGithubUser />} />
+                
             </Routes>
         </div>
             
